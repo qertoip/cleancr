00538e574352114437b5271c8d275c96dae3e9be
Qertoip <qertoip@gmail.com>
2010-08-02 16:50:16 +0200

    lein 1.3 project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,12 @@
+; tested on leiningen 1.3.0-snapshot
+
 (defproject cleancr "0.2"
   :description "Finds and optionally removes CR (carriage return) characters from your project text files."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.1.0"]]
-				 
+
   :jar-dir "dist/"
-  :jar-name "cleancr-small.jar"
+  :jar-name "cleancr-orphan.jar"
   :uberjar-name "cleancr.jar"
   
   :main cleancr)
